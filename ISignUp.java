0738bf4335446f8438985da28de4de5457c35a88
Luyonghao <1185616808@qq.com>
2022-12-08 14:42:58 +0800

    卢永浩-第二次提交

---
+++ ISignUp.java
@@ -0,0 +1,9 @@
+
+public interface ISignUp {
+	public void print();
+	public boolean addStudent (int stuType);
+	public static IParams parse() throws Exception{
+		return null;
+	}
+	boolean addStudent(Integer stuType);
+}
